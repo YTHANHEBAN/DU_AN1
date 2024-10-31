8ad8b67fc63c91d1f9809d3a7d8cef8039b8c68a
YTHANHEBAN <ebanythanh1@gmai.com>
2024-10-31 08:48:56 +0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import ThongTin from './components/thongtin.vue'
+</script>
+
+<template>
+  <header>
+    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
+    <div class="wrapper">
+      <ThongTin mssv="pk03846" ten="YTHANHEBAN" tuoi="22 tuoi" lop="wdk198" sothich="Da Bong" diemmanh="choi the thao" diemyeu="tieng anh kem"/>
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    /* padding-right: calc(var(--section-gap) / 2); */
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/thongtin.vue
@@ -0,0 +1,77 @@
+<script setup>
+defineProps({
+  mssv: {
+    type: String,
+    required: true
+  },
+  ten: {
+    type: String,
+    required: true
+  }
+  ,
+  tuoi: {
+    type: String,
+    required: true
+  }
+  ,
+  lop: {
+    type: String,
+    required: true
+  }
+  ,
+  sothich: {
+    type: String,
+    required: true
+  }
+  ,
+  diemmanh: {
+    type: String,
+    required: true
+  }
+  ,
+  diemyeu: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div >
+    <h1>Giới thiệu thông tin cá nhân</h1>
+    <h2> mssv : {{ mssv }}</h2>
+    <h2>Tên : {{ ten }}</h2>
+    <h2>Tuổi : {{ tuoi }}</h2>
+    <h2>Lop : {{ lop }}</h2>
+    <h2>So Thich : {{ sothich }}</h2>
+    <h2>Diem Manh : {{ diemmanh }}</h2>
+    <h2>Diem Yeu : {{ diemyeu }}</h2>
+  </div>
+
+   
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
